794d9347264b099f765e816937b87c1e74e85bf3
gon <gondo-t@evoltech.co.jp>
2026-06-24 16:06:57 +0900

    ToDoServiceImpl実装クラスの作成

--- src/main/java/com/example/webapp/service/impl/ToDoServiceImpl.java
+++ src/main/java/com/example/webapp/service/impl/ToDoServiceImpl.java
@@ -2,39 +2,49 @@ package com.example.webapp.service.impl;
 
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.example.webapp.entity.ToDo;
+import com.example.webapp.repository.ToDoMapper;
 import com.example.webapp.service.ToDoService;
 
-public class ToDoServiceImpl implements ToDoService {
+import lombok.RequiredArgsConstructor;
 
+/**
+ * ToDo：サービス実装クラス
+ */
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class ToDoServiceImpl implements ToDoService {
+	
+	/** DI */
+	private final ToDoMapper toDoMapper;
+	
 	@Override
 	public List<ToDo> findAllToDo() {
-		// TODO 自動生成されたメソッド・スタブ
-		return null;
+		return toDoMapper.selectAll();
 	}
 
 	@Override
 	public ToDo findByIdToDo(Integer id) {
-		// TODO 自動生成されたメソッド・スタブ
-		return null;
+		return toDoMapper.selectById(id);
 	}
 
 	@Override
 	public void insertToDo(ToDo toDo) {
-		// TODO 自動生成されたメソッド・スタブ
-
+		toDoMapper.insert(toDo);
 	}
 
 	@Override
 	public void updateToDo(ToDo toDo) {
-		// TODO 自動生成されたメソッド・スタブ
-
+		toDoMapper.update(toDo);
 	}
+
 
 	@Override
 	public void deleteToDo(Integer id) {
-		// TODO 自動生成されたメソッド・スタブ
-
+		toDoMapper.delete(id);
 	}
-
 }
