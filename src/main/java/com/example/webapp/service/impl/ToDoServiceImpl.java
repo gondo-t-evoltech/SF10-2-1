7fada727674d78b78f1acf049de7e19aaa2de22b
gon <gondo-t@evoltech.co.jp>
2026-06-24 12:01:32 +0900

    ToDoServiceインターフェースの作成

---
+++ src/main/java/com/example/webapp/service/impl/ToDoServiceImpl.java
@@ -0,0 +1,40 @@
+package com.example.webapp.service.impl;
+
+import java.util.List;
+
+import com.example.webapp.entity.ToDo;
+import com.example.webapp.service.ToDoService;
+
+public class ToDoServiceImpl implements ToDoService {
+
+	@Override
+	public List<ToDo> findAllToDo() {
+		// TODO 自動生成されたメソッド・スタブ
+		return null;
+	}
+
+	@Override
+	public ToDo findByIdToDo(Integer id) {
+		// TODO 自動生成されたメソッド・スタブ
+		return null;
+	}
+
+	@Override
+	public void insertToDo(ToDo toDo) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+	@Override
+	public void updateToDo(ToDo toDo) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+	@Override
+	public void deleteToDo(Integer id) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
