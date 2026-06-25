a00ef4c1a8e2a0e09095704f7166d4591f627b5a
gon <gondo-t@evoltech.co.jp>
2026-06-25 17:03:47 +0900

    動作テスト不要のためWebappApplicationを元に戻す

--- src/main/java/com/example/webapp/WebappApplication.java
+++ src/main/java/com/example/webapp/WebappApplication.java
@@ -3,51 +3,10 @@ package com.example.webapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.example.webapp.entity.ToDo;
-import com.example.webapp.service.ToDoService;
-
-import lombok.RequiredArgsConstructor;
-
 @SpringBootApplication
-@RequiredArgsConstructor
 public class WebappApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WebappApplication.class, args)
-		.getBean(WebappApplication.class).exe();
-	}
-	/** DI */
-	private final ToDoService service;
-	
-	public void exe() {
-		// ★全件検索
-		System.out.println("=== 全件検索 ===");
-		for (ToDo row : service.findAllToDo()) {
-			System.out.println(row);
-		}
-		// ★1件検索
-		System.out.println("=== １件検索 ===");
-		System.out.println(service.findByIdToDo(1));
-		// ★登録
-		// 登録データ作成
-		ToDo todo = new ToDo();
-		todo.setTodo("サービスのテスト");
-		todo.setDetail("ToDo登録サービス");
-		service.insertToDo(todo);
-		System.out.println("=== 登録確認 ===");
-		System.out.println(service.findByIdToDo(4));
-		// ★更新
-		ToDo target = service.findByIdToDo(4);
-		target.setTodo("サービスのテスト：更新");
-		target.setDetail("ToDo更新サービス");
-		service.updateToDo(target);
-		System.out.println("=== 更新確認 ===");
-		System.out.println(service.findByIdToDo(4));
-		// ★削除
-		service.deleteToDo(4);
-		System.out.println("=== 削除確認 ===");
-		for (ToDo row : service.findAllToDo()) {
-			System.out.println(row);
-		}
+		SpringApplication.run(WebappApplication.class, args);
 	}
 }
