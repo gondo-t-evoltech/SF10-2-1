40123a4185ff7023c9ffd2833b0868ca70072f2c
gon <gondo-t@evoltech.co.jp>
2026-06-26 16:35:39 +0900

    ToDoFormクラスにバリデーションを追加

--- src/main/java/com/example/webapp/form/ToDoForm.java
+++ src/main/java/com/example/webapp/form/ToDoForm.java
@@ -1,5 +1,8 @@
 package com.example.webapp.form;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,8 +17,10 @@ public class ToDoForm {
 	/** することID */
 	private Integer id;
 	/** すること */
+	@NotBlank(message = "ToDoは必須です。")
 	private String todo;
 	/** すること詳細 */
+	@Size(min = 1, max =100, message = "詳細は{min}～{max}文字以内で入力してください。")
 	private String detail;
 	/** 新規判定 */
 	private Boolean isNew;
